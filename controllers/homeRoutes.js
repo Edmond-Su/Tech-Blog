563efb9f3c0aa1c2a25476c465179b6dbf49f04d
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-09-11 19:44:13 +1000

    Setup and added code to server.js, config and controllers

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Project, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req,res) => {
+    try{
+        res.render('homepage')
+    }catch(err){
+        res.status(500).json(err)
+    }
+})
+
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+  
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/');
+        return;
+    }
+  
+    res.render('signup');
+});
